c99570ab366c302c85af80d7d738f7806e15108a
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-06-28 04:34:01 +0300

    add search

--- src/main/java/com/example/projectdancestudio/controller/RegistrationController.java
+++ src/main/java/com/example/projectdancestudio/controller/RegistrationController.java
@@ -5,9 +5,13 @@ import com.example.projectdancestudio.service.ClientService;
 import com.example.projectdancestudio.service.EmailService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -30,5 +34,20 @@ public class RegistrationController {
         emailService.sendWelcomeEmail(client);
         return "redirect:/register";
     }
+
+    @GetMapping("/all")
+    public String getAllClient(Model model){
+        model.addAttribute("clients", clientService.FindAddClient());
+        return "allClient";
+    }
+
+    @GetMapping("/search")
+    public String searchClientsByName(@RequestParam String name, Model model) {
+        List<Client> clients = clientService.findClientsByName(name);
+        model.addAttribute("clients", clients);
+        model.addAttribute("searchKeyword", name); // передаем ключевое слово поиска для отображения на странице
+        return "searchClients";
+    }
+
 
 }
--- src/main/java/com/example/projectdancestudio/service/ClientService.java
+++ src/main/java/com/example/projectdancestudio/service/ClientService.java
@@ -5,15 +5,23 @@ import com.example.projectdancestudio.repository.ClientRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class ClientService {
     private final ClientRepository clientRepository;
 
 
-
-
     public Client save(Client client){
        return clientRepository.save(client);
+    }
+
+    public List<Client> FindAddClient(){
+        return clientRepository.findAll();
+    }
+
+    public List<Client> findClientsByName(String name) {
+        return clientRepository.findByNameContainingIgnoreCase(name); // Используйте метод репозитория для поиска клиентов по имени
     }
 }
