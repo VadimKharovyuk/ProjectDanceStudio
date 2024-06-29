b34ce29dc70f6f3b5796becf6af02c215407448a
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-06-29 04:17:57 +0300

    add UserDetailsService

--- src/main/java/com/example/projectdancestudio/Config/SecurityConfig.java
+++ src/main/java/com/example/projectdancestudio/Config/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,6 +22,16 @@ public class SecurityConfig {
                 .anyRequest().authenticated())
                 .formLogin((form)->form.loginPage("/login").permitAll()).logout(LogoutConfigurer::permitAll);
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("q")
+                .password("q")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
     }
 
 }
