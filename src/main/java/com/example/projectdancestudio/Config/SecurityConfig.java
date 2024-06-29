220043324e5fd24dbc51e4b88ebd4ab679a8dc19
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-06-29 04:03:56 +0300

    add SecurityConfig

---
+++ src/main/java/com/example/projectdancestudio/Config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.projectdancestudio.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests((reques)->reques.
+                requestMatchers("/","/register","/pic/**").permitAll()
+                .anyRequest().authenticated())
+                .formLogin((form)->form.loginPage("/login").permitAll()).logout(LogoutConfigurer::permitAll);
+        return http.build();
+    }
+
+}
